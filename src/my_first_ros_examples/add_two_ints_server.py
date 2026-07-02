e520c748f21cf8c83e7fca31adb32c95a5f96bf4
Gyumin_13 <144134793+Gyumin13@users.noreply.github.com>
2026-07-02 14:06:45 +0900

    code: add service server example

---
+++ src/my_first_ros_examples/add_two_ints_server.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from example_interfaces.srv import AddTwoInts
+
+
+class AddTwoIntsServer(Node):
+    def __init__(self):
+        super().__init__('add_two_ints_server')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.handle_request)
+        self.get_logger().info('AddTwoInts server is ready.')
+
+    def handle_request(self, request, response):
+        response.sum = request.a + request.b
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = AddTwoIntsServer()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
