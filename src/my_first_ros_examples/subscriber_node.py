8a9d09ec7a8e89bdf8698f855c18c4b201f73c3b
Gyumin_13 <144134793+Gyumin13@users.noreply.github.com>
2026-07-02 14:06:29 +0900

    code: add subscriber node example

---
+++ src/my_first_ros_examples/subscriber_node.py
@@ -0,0 +1,30 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SubscriberNode(Node):
+    def __init__(self):
+        super().__init__('subscriber_node')
+        self.subscription = self.create_subscription(
+            String,
+            'chatter',
+            self.on_message,
+            10
+        )
+        self.get_logger().info('Subscriber Node has started.')
+
+    def on_message(self, msg):
+        self.get_logger().info(f'I heard: "{msg.data}"')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SubscriberNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
